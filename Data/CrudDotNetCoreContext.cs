076525ccb633e6c9791e2cbe50c5ce12d69de101
Fabiana Angelo <fabianna.info@gmail.com>
2019-10-11 19:12:22 -0300

    Arquivos iniciais

---
+++ Data/CrudDotNetCoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudDotNetCore.Models
+{
+    public class CrudDotNetCoreContext : DbContext
+    {
+        public CrudDotNetCoreContext (DbContextOptions<CrudDotNetCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CrudDotNetCore.Models.Movie> Movie { get; set; }
+    }
+}
