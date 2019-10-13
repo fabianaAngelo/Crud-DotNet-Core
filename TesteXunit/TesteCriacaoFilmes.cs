5d1606022071b40e474b14cdc03d68745883ede8
Fabiana Angelo <fabianna.info@gmail.com>
2019-10-13 18:29:29 -0300

    Retirando a solution de testes.

---
+++ TesteXunit/TesteCriacaoFilmes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TesteXUnit
+{
+    public class TesteCriacaoFilmes
+    {
+        [Theory]
+        public TesteCriacaoFilmes(double a)
+        {
+            double b = 1;
+            var teste = Assert.Equal(a, b);
+
+        }
+    }
+}
